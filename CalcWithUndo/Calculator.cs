99b7cccfadfad1d2fc06969533ada6afbdedbfc3
occulus1975 <occulus1975@gmail.com>
2020-06-08 15:37:11 -0400

    Add project files.

---
+++ CalcWithUndo/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CalcWithUndo
+{
+    class Calculator
+    {
+        
+        //private double _aTerm;
+        //private double _anotherTerm;
+        //private string _anOperator;
+        //private string _aSign;
+
+        //private double _sum;
+        //private double _difference;
+        //private double _product;
+        //private double _quotient;
+        //private double _remainder;
+
+        //private 
+    }
+}
